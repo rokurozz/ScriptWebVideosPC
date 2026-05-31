9e211c08690110a7cdc93864c2e0bc56c76abb63
ROKUROzz <119554809+rokurozz@users.noreply.github.com>
2026-05-31 14:51:56 -0300

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+
+// @name         Script - Botões extras para videos na web - feito para pc 
+
+// @namespace https://github.com/rokurozz/ScriptWebVideos
+
+// @version      1.0.0
+
+// @updateURL    https://raw.githubusercontent.com/rokurozz/ScriptWebVideosPC/PC/script.user.js
+
+// @downloadURL  https://raw.githubusercontent.com/rokurozz/ScriptWebVideosPC/PC/script.user.js
+
+// @description  adiciona alguns botões extras sobre o video na web com salvamento de progresso automático e configuravél via menu pelo usuário
+
+// @match        *://*/*
+
+// @grant        GM_setValue
+
+// @grant        GM_getValue
